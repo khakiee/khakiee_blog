c4c582d3ee39f3b3c29e290176aa43351290d183
khakiee <khakie@vcnc.co.kr>
2023-05-09 17:06:28 +0900

    fix: footer author name

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <Footer class="dark:bg-gray-900">
-  <FooterCopyright href="/" by="Flowbite™" year={2022} />
+  <FooterCopyright href="/" by="khakiee" year={2023} />
   <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
     <FooterLink href="/about">About</FooterLink>
     <FooterLink href="https://github.com/khakiee">github</FooterLink>
