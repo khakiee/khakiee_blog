a8da820f9851eece0bccc4bf4840a056fe6104d3
khakiee <khakie@vcnc.co.kr>
2023-05-12 19:12:35 +0900

    impl: add bio to env

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,7 +3,7 @@
   const nickname = import.meta.env.VITE_USER_NAME
   const githubUrl = import.meta.env.VITE_GITHUB_URL
   const email = import.meta.env.VITE_EMAIL
-  const bio = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+  const bio = import.meta.env.VITE_BIO
 </script>
 
 <div class="flex flex-col items-center justify-center md:min-h-screen min-h-fit md:mt-0 mt-10 bg-gray-100 dark:bg-gray-800 dark:text-gray-200 text-gray-800">
